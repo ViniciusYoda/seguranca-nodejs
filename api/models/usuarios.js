9e6c055a52d065e8e0e7995f256302e14f689e4d
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-24 13:31:15 -0300

    node

--- api/models/usuarios.js
+++ api/models/usuarios.js
@@ -4,9 +4,22 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class usuarios extends Model {
-
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      
+      usuarios.belongsToMany(models.roles, {
+        through: models.usuarios_roles,
+        as: 'usuario_roles',
+        foreignKey: 'usuario_id'
+      })
+      usuarios.belongsToMany(models.permissoes, {
+        through: models.usuarios_permissoes,
+        as: 'usuario_permissoes',
+        foreignKey: 'usuario_id'
+      })
     }
   }
   usuarios.init({
--- api/services/usuarioService.js
+++ api/services/usuarioService.js
@@ -9,50 +9,104 @@ class UsuarioService {
             email: dto.email
          }
       })
+
       if (usuario) {
          throw new Error('Usuario ja cadastrado')
       }
+
       try {
          const senhaHash = await hash(dto.senha, 8)
+
          const novoUsuario = await database.usuarios.create({
             id: uuid.v4(),
             nome: dto.nome,
             email: dto.email,
             senha: senhaHash
          })
+
          return novoUsuario
       } catch (error) {
          throw new Error('Erro ao cadastrar usuario')
       }
+
+
    }
+
    async buscarTodosUsuarios() {
-      const usuarios = await database.usuarios.findAll()
+      const usuarios = await database.usuarios.findAll({
+         include: [
+            {
+               model: database.roles,
+               as: 'usuario_roles',
+               attributes: ['id', 'nome', 'descricao'],
+               through: {
+                  attributes: [],
+               }
+            },
+            {
+               model: database.permissoes,
+               as: 'usuario_permissoes',
+               attributes: ['id', 'nome', 'descricao'],
+               through: {
+                  attributes: [],
+               }
+            }
+         ]
+      })
+
       return usuarios
    }
+
    async buscarUsuarioPorId(id) {
       const usuario = await database.usuarios.findOne({
+         include: [
+            {
+               model: database.roles,
+               as: 'usuario_roles',
+               attributes: ['id', 'nome', 'descricao'],
+               through: {
+                  attributes: [],
+               }
+            },
+            {
+               model: database.permissoes,
+               as: 'usuario_permissoes',
+               attributes: ['id', 'nome', 'descricao'],
+               through: {
+                  attributes: [],
+               }
+            }
+         ],
          where: {
             id: id
          }
       })
+
       if (!usuario) {
          throw new Error('Usuario informado não cadastrado!')
       }
+
       return usuario
    }
+
    async editarUsuario(dto) {
       const usuario = await this.buscarUsuarioPorId(dto.id)
+
       try {
          usuario.nome = dto.nome
          usuario.email = dto.email
+
          await usuario.save()
+
          return usuario
       } catch (error) {
          throw new Error('Erro ao editar usuario!')
       }
    }
+
    async deletarUsuario(id) {
       await this.buscarUsuarioPorId(id)
+
       try {
          await database.usuarios.destroy({
             where: {
@@ -64,4 +118,5 @@ class UsuarioService {
       }
    }
 }
+
 module.exports = UsuarioService
